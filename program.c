900aba3426f2b5b7b6581f84ea93b32387938838
Ayush Srivastava <srivastava_ayushgg2@yahoo.com>
2018-03-15 22:54:21 +0530

    first commit

--- program.c
+++ program.c
@@ -1,1 +1,36 @@
-// program
+#include<stdio.h>
+#include<conio.h>
+
+struct process {
+		int processId ;
+		int priority ;
+		int arrivalTime ;
+		int burstTime ;
+		int responseTime ;
+		int processed ;
+};
+
+struct process queue1[100], queue2[100], queue3[100] ;
+
+void createProcess(int n, struct process proc[]) {	
+	int i ;
+	for(i=0; i<n; i++) {
+		printf("Enter Process Id") ;
+		scanf("%d",&proc[i].processId) ;
+		printf("Enter Process Arrival Time") ;
+		scanf("%d",&proc[i].arrivalTime) ;
+		printf("Enter Process Burst Time") ;
+		scanf("%d",&proc[i].burstTime) ;
+		printf("Enter Processes Priority") ;
+		scanf("%d",&proc[i].priority) ;
+		proc[i].responseTime = proc[i].burstTime ;
+	}
+}
+
+int main() {
+	int n ;
+	printf("How many process - ") ;
+	scanf("%d",&n) ;
+	struct process proc[n] ;
+	createProcess(n, proc) ;
+}
